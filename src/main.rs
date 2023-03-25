a44dc6ffcd10ff098fba374f20931958a6bcffec
sishoo <112181983+sishoo@users.noreply.github.com>
2023-03-25 17:10:25 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,7 @@ use std::mem;
 mod quad_tree;
 use quad_tree::{Node, Rect};
 
-const NUM_BODIES: usize = 1000;
-const SINGLE_BODY_DISTANCE_LIMIT: f32 = 0.0;
-const G: f32 = 6.67430e-11;
-
+const NUM_BODIES: usize = 3;
 
 struct MainState {
     planets: Vec<Planet>,
@@ -23,18 +20,21 @@ struct MainState {
 pub struct Planet {
     pub mass: f32,
     pub pos: Vec2,
+    pub velocity: Vec2,
 }
 
 impl MainState {
     pub fn new() -> Self {
         let mut rng = rand::thread_rng();
         let mut planets = Vec::new();
-        let mut quad_tree = Node::new(Rect::new(Vec2::new(0.0, 0.0), 1000.0, 800.0));
+        let mut quad_tree = Node::new(Rect::new(Vec2::new(0.0, 0.0), 1_000.0, 800.0));
         for _ in 0..NUM_BODIES {
             let planet = Planet {
-                mass: rng.gen_range(1.0..1000000.0),
-                pos: Vec2::new(rng.gen_range(0.0..1000.0), rng.gen_range(0.0..800.0)),
+                mass: rng.gen_range(1.0..1_000_000.0),
+                pos: Vec2::new(rng.gen_range(0.0..1_000.0), rng.gen_range(0.0..800.0)),
+                velocity: Vec2::new(0.0, 0.0)
             };
+
             planets.push(planet.clone());
             quad_tree.insert(planet);
         }
@@ -44,66 +44,33 @@ impl MainState {
             quad_tree: quad_tree,
         }
     }
-
-
-
-
-    fn update_pos(&mut self, planet: Planet) -> Vec2 {
-        let mut x_net_force = 0.0;
-        let mut y_net_force = 0.0;
-        let com_x_difference = self.quad_tree.center_of_mass.x - planet.pos.x;
-        let com_y_difference = self.quad_tree.center_of_mass.y - planet.pos.y;
-        let com_distance = com_x_difference * com_x_difference + com_y_difference * com_y_difference;
-
-        // checks if is node is an external node, meaning if it is a point or not
-        if self.quad_tree.children.is_none() && !self.quad_tree.bounds.bounds_contains(&planet.pos) {
-            let x_difference = self.quad_tree.contents[0].pos.x - planet.pos.x;
-            let y_difference = self.quad_tree.contents[0].pos.y - planet.pos.y;
-            let total_distance = x_difference * x_difference + y_difference * y_difference;
-            let net_force = (G * planet.mass * self.quad_tree.contents[0].mass) / total_distance;
-            x_net_force += net_force * (x_difference / total_distance.sqrt());
-            y_net_force += net_force * (y_difference / total_distance.sqrt());
-        } else {
-            let net_force = (G * planet.mass * self.quad_tree.total_mass) / com_distance;
-            x_net_force += net_force * (com_x_difference / com_distance.sqrt());
-            y_net_force += net_force * (com_y_difference / com_distance.sqrt());
-            println!("{:?}", self.quad_tree.center_of_mass );
-        }
-        println!("{:?}",  Vec2::new(x_net_force, y_net_force));
-        Vec2::new(x_net_force, y_net_force)
-    }
 }
 
 
 impl event::EventHandler for MainState {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
+        for planet in &mut self.planets {
+            println!("velo1 {}", planet.velocity);
+            println!("pos1 {}", planet.pos);
 
-        //while let avg = 449 us
-        //for buffer avg = 333 us
-        // let mut buffer = Vec::new();
-        // while let Some(mut planet) = self.planets.pop() {
-        //     planet.pos += self.update_pos(planet);
-        //     buffer.push(planet);
-        // }
-        // std::mem::swap(&mut buffer, &mut self.planets);
+            self.quad_tree.update_pos(planet);
 
-
-        let now = Instant::now();
-        let mut buffer = Vec::with_capacity(NUM_BODIES);
-        std::mem::swap(&mut buffer, &mut self.planets);
-        for mut planet in buffer {
-            planet.pos += self.update_pos(planet);
-            self.planets.push(planet);
+            println!("velo2 {}", planet.velocity);
+            println!("pos2 {}", planet.pos);
+            println!("________________________________");
         }
-        println!("{:?}", now.elapsed());
+        println!("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        println!("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        println!("CYCLE DONE");
+        println!("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        println!("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult  {
         let mut canvas = graphics::Canvas::from_frame(
             ctx,
-            // graphics::Color::from([0.0, 0.0, 0.0, 0.0])
-            Color::WHITE
+            graphics::Color::from([0.0, 0.0, 0.0, 0.0])
         );
         let planet_mesh = graphics::Mesh::new_circle(
             ctx,
@@ -111,21 +78,8 @@ impl event::EventHandler for MainState {
             Vec2::new(0.0, 0.0),
             3.0,
             0.1,
-            Color::RED,
+            Color::WHITE,
         )?;
-        let black_hole = graphics::Mesh::new_circle(
-            ctx,
-            graphics::DrawMode::fill(),
-            Vec2::new(0.0, 0.0),
-            10.0,
-            0.1,
-            Color::BLACK,
-        )?;
-
-
-        canvas.draw(&black_hole, Vec2::new(500.0, 400.0));
-
-
         for planet in &self.planets {
             canvas.draw(&planet_mesh, planet.pos);
         }
